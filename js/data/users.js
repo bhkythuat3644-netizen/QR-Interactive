294660464101c5ef483db6ba0d39b66092a7f740
bhkythuat3644-netizen <bh.kythuat.3644@gmail.com>
2026-05-04 11:02:23 +0700

    Add files via upload

---
+++ js/data/users.js
@@ -0,0 +1,59 @@
+export const users = [
+{id:"004772", ten:"NGUYỄN THỊ KIM LIÊN", mat_khau:"0000", bp:"THU MUA", xuong:"VPC", ai:1, pkl:1, bk_bed:1, bk_tu:1, bk_spk:0, bk_dg:1, bv_bed:0, bv_tu:0, bv_spk:0, bv_dg:1},
+{id:"004782", ten:"LÊ NGỌC BẢO TRÂM", mat_khau:"0000", bp:"THU MUA", xuong:"VPC", ai:0, pkl:1, bk_bed:0, bk_tu:0, bk_spk:0, bk_dg:1, bv_bed:0, bv_tu:0, bv_spk:0, bv_dg:1},
+{id:"004764", ten:"CÔNG GIA PHỤNG", mat_khau:"0000", bp:"THU MUA", xuong:"VPC", ai:1, pkl:0, bk_bed:0, bk_tu:0, bk_spk:0, bk_dg:0, bv_bed:0, bv_tu:0, bv_spk:0, bv_dg:0},
+{id:"004391", ten:"NGUYỄN THỊ THANH TÂM", mat_khau:"0000", bp:"KẾ TOÁN", xuong:"VPC", ai:0, pkl:0, bk_bed:1, bk_tu:1, bk_spk:0, bk_dg:1, bv_bed:0, bv_tu:0, bv_spk:0, bv_dg:1},
+{id:"003930", ten:"ĐỖ VĂN TÍNH", mat_khau:"0000", bp:"XNK", xuong:"VPC", ai:1, pkl:0, bk_bed:1, bk_tu:1, bk_spk:0, bk_dg:0, bv_bed:0, bv_tu:0, bv_spk:0, bv_dg:0},
+{id:"003894", ten:"NGUYỄN THỊ LAN VIÊN", mat_khau:"0000", bp:"XNK", xuong:"VPC", ai:1, pkl:0, bk_bed:1, bk_tu:1, bk_spk:0, bk_dg:1, bv_bed:0, bv_tu:0, bv_spk:0, bv_dg:1},
+
+{id:"000199", ten:"ĐOÀN VĂN KHANH", mat_khau:"0000", bp:"GC1 TỦ", xuong:"BH", ai:0, pkl:0, bk_bed:0, bk_tu:1, bk_spk:0, bk_dg:0, bv_bed:0, bv_tu:1, bv_spk:0, bv_dg:0},
+{id:"003582", ten:"ĐẶNG NGỌC HÂN", mat_khau:"0000", bp:"GC2 GIƯỜNG", xuong:"BH", ai:0, pkl:0, bk_bed:1, bk_tu:0, bk_spk:0, bk_dg:0, bv_bed:1, bv_tu:0, bv_spk:0, bv_dg:0},
+{id:"003957", ten:"TĂNG THANH DUY", mat_khau:"0000", bp:"KTS", xuong:"BH", ai:0, pkl:0, bk_bed:0, bk_tu:1, bk_spk:0, bk_dg:0, bv_bed:0, bv_tu:1, bv_spk:0, bv_dg:0},
+{id:"004570", ten:"TRẦN PHI LONG", mat_khau:"0000", bp:"KTS", xuong:"BH", ai:0, pkl:0, bk_bed:0, bk_tu:1, bk_spk:0, bk_dg:0, bv_bed:0, bv_tu:1, bv_spk:0, bv_dg:0},
+
+{id:"000067", ten:"NGUYỄN THANH HẢI", mat_khau:"0000", bp:"THÀNH PHẨM", xuong:"BH", ai:1, pkl:0, bk_bed:0, bk_tu:0, bk_spk:0, bk_dg:0, bv_bed:0, bv_tu:0, bv_spk:0, bv_dg:1},
+{id:"000027", ten:"HOÀNG ĐỨC MINH", mat_khau:"0000", bp:"THÀNH PHẨM", xuong:"BH", ai:1, pkl:0, bk_bed:0, bk_tu:0, bk_spk:0, bk_dg:0, bv_bed:0, bv_tu:0, bv_spk:0, bv_dg:1},
+
+{id:"001000", ten:"HOÀNG MINH ĐỨC", mat_khau:"0000", bp:"QC", xuong:"BH", ai:1, pkl:1, bk_bed:1, bk_tu:1, bk_spk:0, bk_dg:1, bv_bed:1, bv_tu:1, bv_spk:0, bv_dg:1},
+
+{id:"000080", ten:"NGUYỄN THỊ NHƯ HOA", mat_khau:"0000", bp:"TƯ LIỆU", xuong:"BH", ai:0, pkl:1, bk_bed:1, bk_tu:1, bk_spk:0, bk_dg:1, bv_bed:0, bv_tu:0, bv_spk:0, bv_dg:1},
+{id:"004084", ten:"PHAN THỊ HƯƠNG GIANG", mat_khau:"0000", bp:"TƯ LIỆU", xuong:"BH", ai:0, pkl:1, bk_bed:1, bk_tu:1, bk_spk:0, bk_dg:0, bv_bed:0, bv_tu:0, bv_spk:0, bv_dg:1},
+{id:"004623", ten:"MAI THỊ LỆ HẰNG", mat_khau:"0000", bp:"TƯ LIỆU", xuong:"BH", ai:0, pkl:0, bk_bed:1, bk_tu:1, bk_spk:0, bk_dg:0, bv_bed:0, bv_tu:0, bv_spk:0, bv_dg:0},
+{id:"003766", ten:"NGUYỄN THỊ PHÚC", mat_khau:"0000", bp:"TƯ LIỆU", xuong:"BH", ai:0, pkl:0, bk_bed:1, bk_tu:1, bk_spk:0, bk_dg:0, bv_bed:0, bv_tu:0, bv_spk:0, bv_dg:0},
+{id:"002920", ten:"LÊ CHÂU THANH", mat_khau:"0000", bp:"TƯ LIỆU", xuong:"BH", ai:0, pkl:1, bk_bed:0, bk_tu:0, bk_spk:0, bk_dg:1, bv_bed:0, bv_tu:0, bv_spk:0, bv_dg:0},
+
+{id:"000029", ten:"NGUYỄN THỊ PHƯỢNG", mat_khau:"0000", bp:"KHO", xuong:"BH", ai:0, pkl:0, bk_bed:1, bk_tu:1, bk_spk:0, bk_dg:0, bv_bed:1, bv_tu:1, bv_spk:0, bv_dg:0},
+{id:"000014", ten:"NGUYỄN HỮU CU", mat_khau:"0000", bp:"TTHK", xuong:"BH", ai:0, pkl:0, bk_bed:0, bk_tu:1, bk_spk:0, bk_dg:0, bv_bed:0, bv_tu:1, bv_spk:0, bv_dg:0},
+{id:"004802", ten:"NGUYỄN HỮU TỨ", mat_khau:"0000", bp:"GC1 TỦ", xuong:"BH", ai:0, pkl:0, bk_bed:0, bk_tu:0, bk_spk:0, bk_dg:0, bv_bed:0, bv_tu:0, bv_spk:0, bv_dg:0},
+
+{id:"004186", ten:"PHẠM XUÂN THƯỜNG", mat_khau:"0000", bp:"VPX", xuong:"BH", ai:0, pkl:0, bk_bed:1, bk_tu:1, bk_spk:0, bk_dg:0, bv_bed:1, bv_tu:1, bv_spk:0, bv_dg:0},
+{id:"004801", ten:"NGUYỄN TÂM ANH", mat_khau:"0000", bp:"VPX", xuong:"BH", ai:0, pkl:0, bk_bed:1, bk_tu:1, bk_spk:0, bk_dg:0, bv_bed:1, bv_tu:1, bv_spk:0, bv_dg:0},
+
+{id:"003866", ten:"ĐỖ VĂN MINH NHÍ", mat_khau:"0000", bp:"VN", xuong:"BH", ai:1, pkl:0, bk_bed:0, bk_tu:0, bk_spk:0, bk_dg:0, bv_bed:0, bv_tu:0, bv_spk:0, bv_dg:1},
+{id:"003925", ten:"TRƯƠNG QUANG TUẤN", mat_khau:"0000", bp:"STĐ", xuong:"BH", ai:1, pkl:0, bk_bed:1, bk_tu:1, bk_spk:0, bk_dg:0, bv_bed:1, bv_tu:1, bv_spk:0, bv_dg:1},
+
+{id:"003949", ten:"NGUYỄN THUYỀN TRƯỞNG", mat_khau:"0000", bp:"CHI TIẾT", xuong:"BH", ai:0, pkl:0, bk_bed:1, bk_tu:1, bk_spk:0, bk_dg:0, bv_bed:1, bv_tu:1, bv_spk:0, bv_dg:0},
+{id:"003984", ten:"TRẦN VĂN ĐỆ", mat_khau:"0000", bp:"KTS", xuong:"BH", ai:0, pkl:0, bk_bed:0, bk_tu:1, bk_spk:0, bk_dg:0, bv_bed:0, bv_tu:1, bv_spk:0, bv_dg:0},
+
+{id:"003512", ten:"TRẦN THỊ TỐ UYÊN", mat_khau:"0000", bp:"KHO VẬT TƯ", xuong:"BH", ai:1, pkl:1, bk_bed:1, bk_tu:1, bk_spk:0, bk_dg:1, bv_bed:1, bv_tu:1, bv_spk:0, bv_dg:1},
+{id:"003473", ten:"YÊN THỊ LAN", mat_khau:"0000", bp:"QC", xuong:"BH", ai:0, pkl:1, bk_bed:1, bk_tu:1, bk_spk:0, bk_dg:1, bv_bed:0, bv_tu:0, bv_spk:0, bv_dg:1},
+{id:"003828", ten:"ĐOÀN THỊ KIM KHÁNH", mat_khau:"0000", bp:"QC", xuong:"BH", ai:0, pkl:1, bk_bed:1, bk_tu:1, bk_spk:0, bk_dg:1, bv_bed:0, bv_tu:0, bv_spk:0, bv_dg:1},
+{id:"004103", ten:"TRƯƠNG TRỌNG TUỆ", mat_khau:"0000", bp:"QC", xuong:"BH", ai:1, pkl:1, bk_bed:1, bk_tu:1, bk_spk:0, bk_dg:1, bv_bed:1, bv_tu:1, bv_spk:0, bv_dg:1},
+{id:"004074", ten:"NGUYỄN THÁI HỌC", mat_khau:"0000", bp:"QC", xuong:"BH", ai:0, pkl:0, bk_bed:1, bk_tu:1, bk_spk:0, bk_dg:0, bv_bed:1, bv_tu:1, bv_spk:0, bv_dg:0},
+
+/* ===== BH2 ===== */
+
+{id:"003937", ten:"KHƯU THƯỢNG ĐỈNH", mat_khau:"0000", bp:"CHÂN GIƯỜNG", xuong:"BH2", ai:0, pkl:0, bk_bed:1, bk_tu:0, bk_spk:0, bk_dg:0, bv_bed:1, bv_tu:0, bv_spk:0, bv_dg:0},
+{id:"003998", ten:"NGUYỄN VĂN DƯ", mat_khau:"0000", bp:"GIƯỜNG KIẾNG", xuong:"BH2", ai:0, pkl:0, bk_bed:0, bk_tu:0, bk_spk:0, bk_dg:0, bv_bed:1, bv_tu:0, bv_spk:0, bv_dg:0},
+{id:"004042", ten:"PHAN THANH ĐỨC", mat_khau:"0000", bp:"VÁN HÔNG MẶT", xuong:"BH2", ai:0, pkl:0, bk_bed:1, bk_tu:1, bk_spk:0, bk_dg:0, bv_bed:1, bv_tu:1, bv_spk:0, bv_dg:0},
+{id:"004049", ten:"TRẦN VĂN ĐỆ", mat_khau:"0000", bp:"VÁN GIƯỜNG", xuong:"BH2", ai:0, pkl:0, bk_bed:1, bk_tu:0, bk_spk:0, bk_dg:0, bv_bed:1, bv_tu:0, bv_spk:0, bv_dg:0},
+{id:"004457", ten:"NGUYỄN VĂN ĐẠT", mat_khau:"0000", bp:"VÁN GIƯỜNG", xuong:"BH2", ai:0, pkl:0, bk_bed:0, bk_tu:0, bk_spk:0, bk_dg:0, bv_bed:1, bv_tu:1, bv_spk:0, bv_dg:0},
+{id:"004025", ten:"HỒ MINH KHANG", mat_khau:"0000", bp:"BỊ LIỆU", xuong:"BH2", ai:0, pkl:0, bk_bed:1, bk_tu:1, bk_spk:0, bk_dg:0, bv_bed:1, bv_tu:1, bv_spk:0, bv_dg:0},
+{id:"004039", ten:"TRẦN VĂN LINH", mat_khau:"0000", bp:"BỊ LIỆU", xuong:"BH2", ai:0, pkl:0, bk_bed:1, bk_tu:1, bk_spk:0, bk_dg:0, bv_bed:1, bv_tu:1, bv_spk:0, bv_dg:0},
+{id:"004160", ten:"LÝ MINH SANG", mat_khau:"0000", bp:"HỘC KÉO", xuong:"BH2", ai:0, pkl:0, bk_bed:1, bk_tu:1, bk_spk:0, bk_dg:0, bv_bed:1, bv_tu:1, bv_spk:0, bv_dg:0},
+{id:"004029", ten:"LÊ THÀNH TRUNG", mat_khau:"0000", bp:"HỘC KÉO", xuong:"BH2", ai:0, pkl:0, bk_bed:1, bk_tu:1, bk_spk:0, bk_dg:0, bv_bed:1, bv_tu:1, bv_spk:0, bv_dg:0},
+{id:"004021", ten:"DƯƠNG THỊ LAN", mat_khau:"0000", bp:"CẮT VÁN GIƯỜNG", xuong:"BH2", ai:0, pkl:0, bk_bed:1, bk_tu:1, bk_spk:0, bk_dg:0, bv_bed:1, bv_tu:1, bv_spk:0, bv_dg:0},
+{id:"003618", ten:"LƯƠNG VĂN LINH", mat_khau:"0000", bp:"QC", xuong:"BH2", ai:0, pkl:0, bk_bed:1, bk_tu:1, bk_spk:0, bk_dg:0, bv_bed:1, bv_tu:1, bv_spk:0, bv_dg:0},
+{id:"003624", ten:"MAI XUÂN TIẾN", mat_khau:"0000", bp:"VPX", xuong:"BH2", ai:0, pkl:0, bk_bed:1, bk_tu:1, bk_spk:0, bk_dg:0, bv_bed:1, bv_tu:1, bv_spk:0, bv_dg:0},
+{id:"004085", ten:"THÁI PHI NHUNG", mat_khau:"0000", bp:"SINH QUẢN", xuong:"BH2", ai:0, pkl:1, bk_bed:1, bk_tu:1, bk_spk:0, bk_dg:1, bv_bed:1, bv_tu:1, bv_spk:0, bv_dg:1}
+];
